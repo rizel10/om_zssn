2c2f016e7361f3a6dafb29d918c643732ed61c82
Lucas Rizel <lucas.wieloch@gmail.com>
2018-03-23 17:01:35 -0300

    finished update_location feature

--- app/controllers/api/v1/survivors_controller.rb
+++ app/controllers/api/v1/survivors_controller.rb
@@ -6,10 +6,20 @@ class Api::V1::SurvivorsController < Api::V1::BaseController
     render json: @survivor
   end
 
+  def update
+    @survivor = Survivor.find(params[:id])
+    @survivor.update!(location_params)
+    render json: @survivor
+  end
+
   ###
   private
 
     def survivor_params
       params.permit(:name, :age, :gender, :lat, :lng, personal_stash: Item.item_types.keys.map { |i| i.to_sym })
+    end
+
+    def location_params
+      params.permit(:lat, :lng)
     end
 end
